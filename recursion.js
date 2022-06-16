a2474660fbfe1c8ebb690fc364044717616a63bd
Abdan Zam Zam Ramadhan <abdan.zamzamramadhan@ai.astra.co.id>
2022-06-17 06:35:22 +0700

    Use Recursion to Create a Countdown

--- recursion.js
+++ recursion.js
@@ -39,3 +39,54 @@ function rangeOfNumbers(startNum, endNum) {
     ? [startNum]
     : [...rangeOfNumbers(startNum, endNum - 1), endNum];
 }
+
+// 2. Use Recursion to Create a Countdown
+
+/**
+ * Notes:
+ * - countdown(-1) should return an empty array.
+ * - countdown(10) should return [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+ * - countdown(5) should return [5, 4, 3, 2, 1]
+ * - Your code should not rely on any kind of loops (for, while or higher order functions such as forEach, map, filter, and reduce).
+ * - You should use recursion to solve this problem.
+ */
+
+// Code:
+
+// Only change code below this line
+function countdown(n) {
+  return;
+}
+// Only change code above this line
+
+// Solution 1
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
+  }
+}
+
+// Solution 2
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const arr = countdown(n - 1);
+    arr.splice(0, 0, n);
+    return arr;
+  }
+}
+
+// Solution 3
+function countdown(n) {
+  return n < 1 ? [] : [n].concat(countdown(n - 1));
+}
+
+// Solution 4
+function countdown(n) {
+  return n < 1 ? [] : [n, ...countdown(n - 1)];
+}
